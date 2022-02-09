83add173c459eacada7b9b2f568ad4314bd6f67e
Cyd Villavicencio <villacv@gmail.com>
2022-02-09 13:00:33 -0500

    added data mongoose connection

--- server/db/index.js
+++ server/db/index.js
@@ -1,7 +1,15 @@
+const items = 'mongodb://127.0.0.1:27017/items'
+const data = 'mongodb://127.0.0.1:27017/data'
 const mongoose = require('mongoose');
 
 mongoose
-  .connect('mongodb://127.0.0.1:27017/items', { useNewUrlParser: true, useFindAndModify: false })
+  .connect(items, { useNewUrlParser: true, useFindAndModify: false })
+  .catch(e => {
+    console.error('Connection error', e.message);
+  });
+
+mongoose
+  .connect(data, { useNewUrlParser: true, useFindAndModify: false })
   .catch(e => {
     console.error('Connection error', e.message);
   });
