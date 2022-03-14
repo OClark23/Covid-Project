a1108450221b6cea4513c92036fe436af68aea95
Cyd Villavicencio <villacv@gmail.com>
2022-03-14 16:23:04 -0400

    test

--- server/db/index.js
+++ server/db/index.js
@@ -1,9 +1,8 @@
-const items = 'mongodb+srv://Frontier:l8KhcHSYBhnWUw9w@cluster0/items?retryWrites=true&w=majority';
 // 'mongodb://127.0.0.1:27017/items'
 const mongoose = require('mongoose');
 
 mongoose
-  .connect(items, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
+  .connect(MONGODB_CONNECTION_STRING, { useNewUrlParser: true, useFindAndModify: false, useUnifiedTopology: true })
   .then(() => console.log("MongoDB has been connected"))
   .catch(e => {
     console.error('Connection error :(', e.message);
